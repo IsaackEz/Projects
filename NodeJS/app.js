baa576575a7f16528f87babb3e9c7feb9bf5d25d
IsaackEz <isaackez@outlook.com>
2020-10-15 03:14:49 -0500

    ModulesFiles

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,5 +1,8 @@
-var logger  = require('./logger');
+const fs = require('fs');
+//const files = fs.readdirSync('./');
+//console.log(files);
 
-console.log(logger);
-
-logger.log('message');
+fs.readdir('$', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
