d3de5ebeff5a5f18b0f32d07f10cee9ad22cbf4e
IsaackEz <isaackez@outlook.com>
2020-10-15 12:39:53 -0500

    Arrays

--- JS-BASICS/index.js
+++ JS-BASICS/index.js
@@ -1,23 +1,4 @@
-let isApproved = true;
-let firstName;
-let selectedColor = null;
-
-let person = {
-    name: 'Isaac',
-    age: 21
-};
-
-//Dot Notation
-person.name = 'Carlos';
-
-//Bracket Notation
-//person['name'] = 'April';
-let selection = 'name';
-person[selection] = 'April';
-
-console.log(person.name);
-
-//Cannot be a reserved keyword
-//Cannot start with a number
-//Cannot contain a space or hyphen (-)
-//Are case-sensitive
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 2;
+//An array is also an object
+console.log(selectedColors.length);
